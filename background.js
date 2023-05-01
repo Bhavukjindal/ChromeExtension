4a46453a0566e87d1a01478604d6748ff9fc1784
Bhavuk <bhavukjindal123@gmail.com>
2023-05-01 17:30:28 +0530

    changes except problemCount

--- background.js
+++ background.js
@@ -1,15 +1,58 @@
 // import fetchLocation from "./fetchLocation.js";
 
-const endpt = "http://localhost:3000/api/v1/problems";
+const endptForProblem = "http://localhost:3000/api/v1/problems";
+const endptForUser = "http://localhost:3000/api/v1/users";
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.message === "callApi") {
-    fetch(endpt,{
+  if (request.apiFor === "Problem") {
+    let bodyForProblem = {
+      "problemId" : request.problemId,
+      "previousEmoji" : request.prevEmoji,
+      "currentEmoji" : request.currentEmoji
+    }
+    console.log(request.prevEmoji);
+    console.log(JSON.stringify(bodyForProblem));
+    fetch(endptForProblem,{
       method: 'PATCH',
       headers: {'Content-Type': 'application/json','Accept': 'application/json',},
-      body: JSON.stringify(postValues),
+      body: JSON.stringify(bodyForProblem),
     })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // send response back to content.js
+        sendResponse({ message: "apiResponse", data: data });
+      })
+      .catch(error => {
+        console.error(error);
+        // send error response back to content.js
+        sendResponse({ message: "apiError", error: error });
+      });
+    // return true to indicate that you will send a response asynchronously
+    return true;
+  }else{
+    let bodyForUser = {
+      "problemId" : request.problemId,
+      "userId" : request.userId,
+      "currentEmoji" : request.currentEmoji
+    }
+    
+    console.log(JSON.stringify(bodyForUser));
+    fetch(endptForUser,{
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json','Accept': 'application/json',},
+      body: JSON.stringify(bodyForUser),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then(data => {
         // send response back to content.js
         sendResponse({ message: "apiResponse", data: data });
--- contentScript.js
+++ contentScript.js
@@ -2,11 +2,55 @@
 
 (() => 
 {
-  // Get the current URL
-  const currentUrl = window.location.href;
-  let accountClassDiv = document.getElementsByClassName("lang-chooser")[0];
-  let checkLogin = accountClassDiv.querySelectorAll('div');
-  let selectedButton = "";
+    // Get the current URL
+    const currentUrl = window.location.href;
+    let accountClassDiv = document.getElementsByClassName("lang-chooser")[0];
+    let checkLogin = accountClassDiv.querySelectorAll('div');
+    let selectedButton = "";
+    let [boringCount, niceCount, amazingCount] = [0, 0, 0];
+    let buttonTypes = {
+      "boring": 0,
+      "nice": 1,
+      "amazing": 2
+    }
+  
+  let loggedin = false;
+
+  if (checkLogin.length === 2) {
+    loggedin = false;
+  } else {
+    loggedin = true;
+  }
+
+
+  let tableDataofReactions = "";
+  let emojiButtons = "";
+
+  let newRow = `<tr>
+                  <td class="left bottom" colspan="1">
+                    <span class="contest-state-phase"><span>
+                    <button style="font-size:30px;background-color:aliceblue" class="boring" id="boring emoji" title="boring">&#128542;</button>
+                    <button style="font-size:30px;background-color:aliceblue" class="nice" id="nice emoji" title="nice">&#128516;</button>
+                    <button style="font-size:30px;background-color:aliceblue" class="amazing" id="amazing emoji" title="amazing">&#128525;</button>
+                  </td>
+                </tr>`;
+
+  if (loggedin) {
+    let tableBody = document.getElementsByClassName('rtable')[0].querySelector('tbody');
+    let bottomMostElement = tableBody.querySelectorAll('tr')[tableBody.querySelectorAll('tr').length - 1];
+    let tableData = bottomMostElement.querySelectorAll('td')[0];
+    // remove class bottom from tableData
+    tableData.classList.remove('bottom');
+    // add innerHTML as table row in tableBody
+    tableBody.innerHTML += newRow;
+
+    tableDataofReactions = document.getElementsByClassName('left bottom')[0];
+    // console.log(tableDataofReactions);
+    emojiButtons = tableDataofReactions.querySelectorAll('button');
+  }
+
+
+
 
   function isdigit(str) {
     return /^\d+$/.test(str);
@@ -24,7 +68,7 @@
     }
   }
 
-  let problemName = document.getElementsByClassName("title")[0].innerText.split(' ')[1];
+  let problemName = document.getElementsByClassName("title")[0].innerText.split('. ')[1];
   console.log(problemName);
 
   let problemId = contestId + "-" + problemName;
@@ -51,55 +95,43 @@
         // Process the response data
         currentUserData = data;
         console.log(data);
+        selectedButton = emojiButtons[buttonTypes[data.emoji]];
+        if(selectedButton){
+          selectedButton.style["background-color"] = "green";
+        }
       })
       .catch(error => {
         console.error(error);
       });
+
+      // fetch(`http://localhost:3000/api/v1/users?problemId=${problemId}`, {
+      //   method: 'GET',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Accept': 'application/json',
+      //   }
+      //   })
+      //   .then(response =>{
+      //     if (!response.ok) {
+      //       throw new Error('Network response was not ok');
+      //     }
+      //     return response.json();
+      //   })
+      //   .then(data => {
+      //     // Process the response data
+      //     console.log(data);
+      //     [boringCount, niceCount, amazingCount] = [data[0], data[1], data[2]];
+      //     console.log("setValues",boringCount, niceCount, amazingCount);
+      //   })
+      //   .catch(error => {
+      //     console.error(error);
+      //   });
   }
-  
 
-  let loggedin = false;
-
-  if (checkLogin.length === 2) {
-    loggedin = false;
-  } else {
-    loggedin = true;
-  }
-
-
-  let tableDataofReactions = "";
-  let emojiButtons = "";
-
-  let newRow = `<tr>
-                  <td class="left bottom" colspan="1">
-                    <span class="contest-state-phase"><span>
-                    <button style="font-size:30px;" class="boring" id="boring emoji" title="boring">&#128542;</button>
-                    <button style="font-size:30px" class="nice" id="nice emoji" title="nice">&#128516;</button>
-                    <button style="font-size:30px" class="amazing" id="amazing emoji" title="amazing">&#128525;</button>
-                  </td>
-                </tr>`;
-
-  if (loggedin) {
-    let tableBody = document.getElementsByClassName('rtable')[0].querySelector('tbody');
-    let bottomMostElement = tableBody.querySelectorAll('tr')[tableBody.querySelectorAll('tr').length - 1];
-    let tableData = bottomMostElement.querySelectorAll('td')[0];
-    // remove class bottom from tableData
-    tableData.classList.remove('bottom');
-    // add innerHTML as table row in tableBody
-    tableBody.innerHTML += newRow;
-
-    tableDataofReactions = document.getElementsByClassName('left bottom')[0];
-    // console.log(tableDataofReactions);
-    emojiButtons = tableDataofReactions.querySelectorAll('button');
-    // console.log(emojiButtons[0]);
-
-    emojiButtons[1].style["background-color"] = "aliceblue";
-  }
-  
-  const button = emojiButtons[0];
-  button.addEventListener("click", function() {
-    // Send a message to background.js to call the API
-    chrome.runtime.sendMessage({ message: "callApi" }, function(response) {
+  function handleButtonClickEventForProblem(selectedButton,newButton){
+    newButton.style["background-color"] = "green";
+    selectedButton.style["background-color"] = "aliceblue";
+    chrome.runtime.sendMessage({ problemId : problemId,currentEmoji: newButton.classList[0], prevEmoji : selectedButton.classList[0] , apiFor : "Problem" }, function(response) {
       // Handle the API response or error message
       if (response.message === "apiResponse") {
         console.log("API response:", response.data);
@@ -107,8 +139,48 @@
         console.error("API error:", response.error);
       }
     });
+  }
+
+  function handleButtonClickEventForUser(selectedButton,newButton){
+    chrome.runtime.sendMessage({ problemId : problemId,currentEmoji: newButton.classList[0], userId : userId , apiFor : "user" }, function(response) {
+      // Handle the API response or error message
+      if (response.message === "apiResponse") {
+        console.log("API response:", response.data);
+      } else if (response.message === "apiError") {
+        console.error("API error:", response.error);
+      }
+    });
+  }
+  
+  
+  const boringButton = emojiButtons[0];
+  boringButton.addEventListener("click", function() {
+    // Send a message to background.js to call the API
+    if(selectedButton!=boringButton){
+      handleButtonClickEventForProblem(selectedButton,boringButton)
+      handleButtonClickEventForUser(selectedButton,boringButton)
+      selectedButton = boringButton;
+    }
   });
 
+  const niceButton = emojiButtons[1];
+  niceButton.addEventListener("click", function() {
+    if(selectedButton!=niceButton){
+      handleButtonClickEventForProblem(selectedButton,niceButton)
+      handleButtonClickEventForUser(selectedButton,niceButton)
+      selectedButton = niceButton;
+    
+    }
+  });
+
+  const amazingButton = emojiButtons[2];
+  amazingButton.addEventListener("click", function() {
+    if(selectedButton!=amazingButton){
+      handleButtonClickEventForProblem(selectedButton,amazingButton)
+      handleButtonClickEventForUser(selectedButton,amazingButton)
+      selectedButton = amazingButton;
+    }
+  });
 
 }
 )();
